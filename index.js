8ba53b3287f43c334510bbe55385e931cb09185f
Scott <sbferguson17@gmail.com>
2022-07-07 11:32:21 -0600

    company/position/location data added

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        // Typical action to be performed when the document is ready:
-       document.getElementById("container").innerHTML = xhttp.responseText;
+       var data = JSON.parse(xhttp.responseText)
+       var container = document.getElementById("container");
+       data.slice(1).forEach((row, i) => {
+        var rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+        rowDiv.innerHTML = `
+                <h5 class="company">${row.company}</h5>
+                <h3 class="position">${row.position}</h3>
+                <p class="location">${row.location}</p>
+        `;
+        container.appendChild(rowDiv);
+       });
     }
 };
 xhttp.open("GET", "https://remoteok.com/api", true);
